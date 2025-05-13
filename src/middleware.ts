058f29ebe066952def433849377f59f79f87cb58
Coochiee <sugarytchannel@gmail.com>
2025-05-13 19:20:39 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
   }
 
   if (!token && url.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/signin', request.url));
+    return NextResponse.redirect(new URL('/api/auth/signin', request.url));
   }
 
   return NextResponse.next();
